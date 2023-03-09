03eadfe5209fea58179be3d990e514709c4eb63a
Dave Ames <david.john.ames@gmail.com>
2023-03-09 13:04:34 +0000

    Made it so that the tests are run and display failures in the same order that they are defined in.

--- src/test/java/com/booleanuk/extension/ExtensionTest.java
+++ src/test/java/com/booleanuk/extension/ExtensionTest.java
@@ -1,8 +1,12 @@
 package com.booleanuk.extension;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ExtensionTest {
     Extension extension;
 
@@ -11,21 +15,25 @@ public class ExtensionTest {
     }
 
     @Test
+    @Order(1)
     public void helloWorldShouldBeAppended() {
         Assertions.assertEquals("Hello, world!", this.extension.one().toString());
     }
 
     @Test
+    @Order(2)
     public void helloWorldShouldBeReversed() {
         Assertions.assertEquals("!dlrow ,olleH", this.extension.two().toString());
     }
 
     @Test
+    @Order(3)
     public void commaShouldBeRemoved() {
         Assertions.assertEquals("Hello world!", this.extension.three().toString());
     }
 
     @Test
+    @Order(4)
     public void shouldSayHelloToJava() {
         Assertions.assertEquals("Hello, Java!", this.extension.four().toString());
     }
