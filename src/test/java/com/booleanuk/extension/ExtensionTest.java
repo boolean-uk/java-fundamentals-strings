31e1fcc52ad552d14416791f0de8f6d261202d32
Dave Ames <david.john.ames@gmail.com>
2023-03-16 10:07:01 +0000

    Revert "Made it so that the tests are run and display failures in the same order that they are defined in."
    This reverts commit 03eadfe5209fea58179be3d990e514709c4eb63a.

--- src/test/java/com/booleanuk/extension/ExtensionTest.java
+++ src/test/java/com/booleanuk/extension/ExtensionTest.java
@@ -1,12 +1,8 @@
 package com.booleanuk.extension;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ExtensionTest {
     Extension extension;
 
@@ -15,25 +11,21 @@ public class ExtensionTest {
     }
 
     @Test
-    @Order(1)
     public void helloWorldShouldBeAppended() {
         Assertions.assertEquals("Hello, world!", this.extension.one().toString());
     }
 
     @Test
-    @Order(2)
     public void helloWorldShouldBeReversed() {
         Assertions.assertEquals("!dlrow ,olleH", this.extension.two().toString());
     }
 
     @Test
-    @Order(3)
     public void commaShouldBeRemoved() {
         Assertions.assertEquals("Hello world!", this.extension.three().toString());
     }
 
     @Test
-    @Order(4)
     public void shouldSayHelloToJava() {
         Assertions.assertEquals("Hello, Java!", this.extension.four().toString());
     }
