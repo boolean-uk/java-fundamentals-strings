f11028c12343ca82f515c1239c4433e01ae7878e
Rose <rosevandergiessen@proton.me>
2023-04-26 16:29:48 +0200

    Core complete

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -29,6 +29,7 @@ public class Extension {
         // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
         // WRITE YOUR CODE BETWEEN THIS LINE...
 
+        sb.append("Hello, world!");
 
 
         // ...AND THIS LINE
@@ -43,7 +44,7 @@ public class Extension {
         // 2. After adding the message, use an appropriate StringBuilder method to reverse it
         // WRITE YOUR CODE BETWEEN THIS LINE...
 
-
+        sb.append("Hello, world!").reverse();
 
         // ...AND THIS LINE
 
@@ -57,7 +58,7 @@ public class Extension {
         // 2. After adding the message, remove the comma.
         // WRITE YOUR CODE BETWEEN THIS LINE...
 
-
+        sb.append("Hello, world!").deleteCharAt(5);
 
         // ...AND THIS LINE
 
@@ -71,7 +72,7 @@ public class Extension {
         // 2. After adding the message, replace the word "world" with the word "Java"
         // WRITE YOUR CODE BETWEEN THIS LINE...
 
-
+        sb.append("Hello, world").replace(7, 12, "Java!");
 
         // ...AND THIS LINE
 
