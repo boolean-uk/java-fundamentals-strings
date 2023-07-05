5b1225438cd430e5586b485ef141e49a425bc525
PiotrSadolewski <piotr.sadolewski00@gmail.com>
2023-07-05 14:09:21 +0200

    done last 2 extension exercises

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -58,9 +58,11 @@ public class Extension {
         // 2. After adding the message, remove the comma.
         // WRITE YOUR CODE BETWEEN THIS LINE...
 
+        sb.append("Hello, world!");
+        int coma = sb.indexOf(",");
+        sb.deleteCharAt(coma);
 
-
-        // ...AND THIS LINE
+                // ...AND THIS LINE
 
         return sb;
     }
@@ -71,7 +73,11 @@ public class Extension {
         // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
         // 2. After adding the message, replace the word "world" with the word "Java"
         // WRITE YOUR CODE BETWEEN THIS LINE...
+        sb.append("Hello, world!");
+        int start = sb.indexOf("world");
+        int end = "world".length();
 
+        sb.replace(start, start+end, "Java");
 
 
         // ...AND THIS LINE
